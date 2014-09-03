81dd6a7a7c2ca41a50691f8d6130df824d4d8801
Justin T <justin@em.ails.us>
2014-09-03 09:43:18 -0500

    Fix dump file naming

--- foreman_host_dump.sh
+++ foreman_host_dump.sh
@@ -10,10 +10,10 @@ printf "\tPlease wait while dumping Foreman hosts... "
 
 cd ~/tmp
 myAccount=`cat password_file`
->foreman_dump
+cat /dev/null > foreman_dump.txt
 for cntr in {0..1000}; do
-printf "\n\n" >> foreman_dump
-curl -s -k -u $myAccount https://foreman.dmotorworks.com/api/v2/hosts/$cntr/ >> foreman_dump.txt
+  printf "\n\n\n" >> foreman_dump.txt
+  curl -s -k -u $myAccount https://foreman.dmotorworks.com/api/v2/hosts/$cntr/ >> foreman_dump.txt
 done
 
 printf "done\n\n"
