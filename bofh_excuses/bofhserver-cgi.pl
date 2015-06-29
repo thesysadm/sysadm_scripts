aa0d65dc55c92413c0c53c260ce2d66685f42d82
Justin T <justin@em.ails.us>
2015-06-29 10:39:49 -0500

    Update the BOFH generator for my means

--- bofh_excuses/bofhserver-cgi.pl
+++ bofh_excuses/bofhserver-cgi.pl
@@ -1,10 +1,14 @@
-#!/s/std/bin/perl
+#!/bin/perl
 
 #
 # (c) 1994-2000 Jeff Ballard.
 #
+##
+## Modified by J Tianen 20150629
+##
 
-open(F, "/u/b/a/ballard/public/html/bofh/excuses") || print "Content-type: text/html\n\nAck...can't read the excuse file! Don't expect the rest to work.\n"; 
+# Open the excuses file and grab one at "random".
+open(F, "/srv/http/vhosts/sysadm.pw/bofh/excuses") or die "Content-type: text/html\n\nAck...can't read the excuse file! Don't expect the rest to work.\n";
 
 srand(time);
 
@@ -16,23 +20,18 @@ while( $excuse[$number] = <F>) {
 
 $thisexcuse = $excuse[ (rand(1000)*$$)%($number+1) ];
 
+
+## Display the results
 print "Content-type: text/html\n";
 print "\n";
 print "<title>\"Bastard Operator From Hell\"-Style Excuses</title>\n";
-
-print "<center><font size = \"+2\">";
-print "<a href = \"http://www.cs.wisc.edu/~ballard/bofh/\">";
-print "\"The Bastard Operator From Hell\"-style excuse server.";
-print "</a></font>";
-print "<Hr><br>";
+print "<center><font size = \"+2\">The \"Bastard Operator From Hell\"-style excuse generator.</font>";
+print "<br><br><hr><br>";
 print "The cause of the problem is:<br>";
 print "<font size = \"+2\">";
 print "$thisexcuse</font>";
-print "<BR><BR><HR>";
-print "The BOFH-style excuse generator brought to you by";
-print " <a href = \"http://www.cs.wisc.edu/~ballard/\">";
-print "Jeff Ballard</a>.";
+print "<br><br><hr>";
 
+## Exit gracefully
 exit(0);
-
 
